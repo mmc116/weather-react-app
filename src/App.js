3cc6bea2834aad1159bce83e894288c0c162c30e
mmc116 <mmc116@users.noreply.github.com>
2020-10-27 01:41:14 +0000

    Updated footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App </h1>
-      <footer>This project was coded by <a href="https://www.linkedin.com/in/marta-chaves-aa1b271b2/" target="_blank"> Marta Chaves</a> and is open-sourced on <a href="https://github.com/mmc116/weather-react-app" target="_blank"> GitHub </a> . </footer>
+      <footer>This project was coded by <a href="https://www.linkedin.com/in/marta-chaves-aa1b271b2/" target="_blank" rel="noopener noreferrer"> Marta Chaves</a> and is open-sourced on <a href="https://github.com/mmc116/weather-react-app" target="_blank" rel="noopener noreferrer"> GitHub </a> . </footer>
     </div>
   );
 }
